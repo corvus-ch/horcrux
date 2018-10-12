ba8766a84c4ca0918f1b82ef5c5030433486b78f
Christian Haeusler <haeusler.christian@mac.com>
2018-10-12 23:07:25 +0200

    Export format struct

--- format/raw/raw.go
+++ format/raw/raw.go
@@ -5,34 +5,34 @@ import (
 	"io"
 )
 
-// Name holds the name of the format.
+// Name holds the name of the Format.
 const Name = "raw"
 
-// New creates a new format of type raw.
-func New(stem string) *format {
-	return &format{stem: stem}
+// New creates a new Format of type raw.
+func New(stem string) *Format {
+	return &Format{stem: stem}
 }
 
-type format struct {
+type Format struct {
 	stem string
 }
 
 // OutputFileName returns the file name for the given x.
-func (f *format) OutputFileName(x byte) string {
+func (f *Format) OutputFileName(x byte) string {
 	return fmt.Sprintf("%s.raw.%03d", f.stem, x)
 }
 
-// Writer creates a new format writer using the given writer as output.
-func (f *format) Writer(out io.Writer) (io.Writer, []io.Closer, error) {
+// Writer creates a new Format writer using the given writer as output.
+func (f *Format) Writer(out io.Writer) (io.Writer, []io.Closer, error) {
 	return out, nil, nil
 }
 
-// Reader creates a new format reader using the given reader as input.
-func (f *format) Reader(r io.Reader) (io.Reader, error) {
+// Reader creates a new Format reader using the given reader as input.
+func (f *Format) Reader(r io.Reader) (io.Reader, error) {
 	return r, nil
 }
 
 // Name returns the formats name.
-func (f *format) Name() string {
+func (f *Format) Name() string {
 	return Name
 }
