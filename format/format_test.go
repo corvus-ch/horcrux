1076b29cdea111a30c8230da3b9988c8b595db32
Christian Haeusler <haeusler.christian@mac.com>
2018-10-13 11:55:27 +0200

    Test new format

---
+++ format/format_test.go
@@ -0,0 +1,29 @@
+package format_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/corvus-ch/horcrux/format"
+	"github.com/corvus-ch/horcrux/format/raw"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNew(t *testing.T) {
+	tests := []struct {
+		name string
+		err  error
+	}{
+		{raw.Name, nil},
+		{"foo", fmt.Errorf("unknown format foo")},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			f, err := format.New(test.name, "")
+			assert.Equal(t, test.err, err)
+			if f != nil {
+				assert.Equal(t, test.name, f.Name())
+			}
+		})
+	}
+}
