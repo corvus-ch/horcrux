488676909db4333e05e0bfd5bd1bc92825711ed2
Christian Haeusler <haeusler.christian@mac.com>
2018-10-19 18:36:48 +0200

    Correctly comment tokens

--- format/text/token.go
+++ format/text/token.go
@@ -5,13 +5,22 @@ type token int
 
 const (
 	// Special tokens
-	EOF token = iota // End of file
-	EOL              // End of line
-	WS               // White space
+
+	// EOF denotes the end of a file.
+	EOF token = iota
+	// EOL denotes the end of a line.
+	EOL
+	// WS denotes any whitespace except EOF and EOL.
+	WS
 
 	// Literals
-	CRC  // Checksum
-	DATA // Payload data
-	DESC // Arbitrary description
-	LINO // Line number
+
+	// CRC denotes a crc24 checksum.
+	CRC
+	// DATA denotes the zbase32 encoded payload data.
+	DATA
+	// LINO denotes a line number.
+	LINO
+	// DESC denotes any other type of text.
+	DESC
 )
