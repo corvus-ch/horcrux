3a8afad88897be77a90d907f6e0d85556aec4d7c
Christian Haeusler <haeusler.christian@mac.com>
2018-11-10 13:39:54 +0100

    Document the checksum hash

--- input/hash.go
+++ input/hash.go
@@ -12,23 +12,43 @@ import (
 	"golang.org/x/crypto/ripemd160"
 )
 
+const (
+	// MD5 holds the identifier of the MD5 algorithm.
+	MD5 = "md5"
+
+	// SHA1 holds the identifier of the SHA1 algorithm.
+	SHA1 = "sha1"
+
+	// SHA256 holds the identifier of the SHA256 algorithm.
+	SHA256 = "sha256"
+
+	// SHA512 holds the identifier of the SHA512 algorithm.
+	SHA512 = "sha512"
+
+	// RIPEMD160 holds the identifier of the RIPEMD160 algorithm.
+	RIPEMD160 = "ripemd160"
+)
+
+// Hash represents a set of input checksums calculated with several algorithms.
 type Hash struct {
 	algs   map[string]hash.Hash
 	closed bool
 }
 
+// NewHash returns a new instance of Hash with pre populated algorithms.
 func NewHash() *Hash {
 	return &Hash{
 		algs: map[string]hash.Hash{
-			"md5":       md5.New(),
-			"sha1":      sha1.New(),
-			"sha256":    sha256.New(),
-			"sha512":    sha512.New(),
-			"ripemd160": ripemd160.New(),
+			MD5:       md5.New(),
+			SHA1:      sha1.New(),
+			SHA256:    sha256.New(),
+			SHA512:    sha512.New(),
+			RIPEMD160: ripemd160.New(),
 		},
 	}
 }
 
+// Write implements io.Writer and updates the all the checksums.
 func (h *Hash) Write(p []byte) (n int, err error) {
 	for _, w := range h.algs {
 		n, err = w.Write(p)
@@ -43,31 +63,40 @@ func (h *Hash) Write(p []byte) (n int, err error) {
 	return len(p), nil
 }
 
+// Close implements io.Closer.
+// Checksums will not be considered valid until Close() got
+// called.
 func (h *Hash) Close() error {
 	h.closed = true
 	return nil
 }
 
+// Md5 returns the inputs MD5 checksums.
 func (h *Hash) Md5() string {
-	return h.Sum("md5")
+	return h.Sum(MD5)
 }
 
+// Sha1 returns the inputs SHA1 checksums.
 func (h *Hash) Sha1() string {
-	return h.Sum("sha1")
+	return h.Sum(SHA1)
 }
 
+// Sha256 returns the inputs SHA256 checksums.
 func (h *Hash) Sha256() string {
-	return h.Sum("sha256")
+	return h.Sum(SHA256)
 }
 
+// Sha512 returns the inputs SHA512 checksums.
 func (h *Hash) Sha512() string {
-	return h.Sum("sha512")
+	return h.Sum(SHA512)
 }
 
+// Ripemd160 returns the inputs RIPEMD160 checksums.
 func (h *Hash) Ripemd160() string {
-	return h.Sum("ripemd160")
+	return h.Sum(RIPEMD160)
 }
 
+// Sum returns the inputs checksums for the given algorithm.
 func (h *Hash) Sum(a string) string {
 	if !h.closed {
 		return "undefined"
