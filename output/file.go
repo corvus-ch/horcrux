8c6d5ecaf35414b3271f58882b99a76b5d960257
Christian Haeusler <794584+corvus-ch@users.noreply.github.com>
2018-12-02 14:02:48 +0100

    Improve documentation of the output package

--- output/file.go
+++ output/file.go
@@ -19,7 +19,7 @@ type File interface {
 	// Size returns the size of the output file in bytes.
 	Size() int64
 
-	// Checksum returns the inputs checksum calculated with the given algorithm.
+	// Checksum returns the outputs checksum calculated with the given algorithm.
 	Checksum(alg string) (string, error)
 
 	// Meta returns the mata data attached with the file for the given key.
@@ -33,6 +33,7 @@ type file struct {
 	meta map[string]interface{}
 }
 
+// NewFile creates a new instance of File for the given path.
 func NewFile(path string, meta map[string]interface{}) File {
 	return &file{
 		path: path,
@@ -41,14 +42,17 @@ func NewFile(path string, meta map[string]interface{}) File {
 	}
 }
 
+// Name returns the outputs file name.
 func (f *file) Name() string {
 	return filepath.Base(f.path)
 }
 
+// Path returns the outputs file path.
 func (f *file) Path() string {
 	return f.path
 }
 
+// Size returns the outputs size. If the size can not be determined, the value will be -1.
 func (f *file) Size() int64 {
 	if f.size < 0 {
 		if fi, err := os.Stat(f.path); err == nil {
@@ -59,6 +63,8 @@ func (f *file) Size() int64 {
 	return f.size
 }
 
+// Checksum returns the outputs checksum calculated with the given algorithm.
+// If the checksum can not be calculated, the returned value will be 'undefined'.
 func (f *file) Checksum(alg string) (string, error) {
 	if f.hash == nil {
 		cs, err := hash.NewHashForPath(f.path)
@@ -70,16 +76,21 @@ func (f *file) Checksum(alg string) (string, error) {
 	return f.hash.Sum(alg)
 }
 
+// Meta returns a piece of meta data attached to the output and identified by the given key.
 func (f *file) Meta(key string) (string, error) {
 	if f.meta == nil {
-		return "", fmt.Errorf("no meta data is known with key %s", key)
+		return "", metadataKeyUnknown(key)
 	}
 
 	val, ok := f.meta[key]
 
 	if !ok {
-		return "", fmt.Errorf("no meta data is known with key %s", key)
+		return "", metadataKeyUnknown(key)
 	}
 
 	return fmt.Sprint(val), nil
+}
+
+func metadataKeyUnknown(key string) error {
+	return fmt.Errorf("no meta data is known with key %s", key)
 }
