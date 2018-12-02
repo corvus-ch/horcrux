8a5c5cc88da8bfe7b6dcb6872e9ddd204a685bd2
Christian Haeusler <794584+corvus-ch@users.noreply.github.com>
2018-12-02 13:25:44 +0100

    Document template data

--- format/text/data.go
+++ format/text/data.go
@@ -7,6 +7,7 @@ import (
 	"github.com/corvus-ch/horcrux/output"
 )
 
+// Data holds the values passed to the template.
 type Data struct {
 	Input input.Input
 	Lines chan Line
@@ -14,17 +15,20 @@ type Data struct {
 	files output.Output
 }
 
+// NewData creates a new Daa instance.
 func NewData(in input.Input, x byte, out output.Output) *Data {
-	d := &Data{
+	return &Data{
 		Input: in,
 		Lines: make(chan Line),
 		X:     x,
 		files: out,
 	}
-
-	return d
 }
 
+// OutputFiles returns the list of generates files for a given output format.
+//
+// Since the list might not be known at the time this function gets called, a channel is returned.
+// Files will be written to that channel and the channel will be closed when no new files have to be expected.
 func (d *Data) OutputFiles(format string) (files chan output.File, err error) {
 	files, ok := d.files[format]
 	if !ok {
